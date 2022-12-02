aefb9f146725590f47c38294acadaf4351d8077b
setyawannn <prayogasetyawan.adi@gmail.com>
2022-12-03 06:15:13 +0700

    setState initialization

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
-  // constructor() {
-  //   super();
-  // }
+  constructor() {
+    super();
+
+    this.state = {
+      sisaUang: 0,
+      persentasiUang: 0,
+      pemasukanUang: 0,
+      pengeluaranUang: 0,
+      transaksiIN: 0,
+      transaksiOUT: 0,
+      summary: [
+        {
+          deskripsi: "Menerima gaji",
+          tanggal: '1 July 2022',
+          nominal: 1000000,
+          kategori: 'IN',
+        },
+        {
+          deskripsi: "Makan nasi padadng",
+          tanggal: '2 July 2022',
+          nominal: 20000,
+          kategori: 'OUT',
+        },
+      ],
+    }
+
+  }
 
   render() {
     return (
@@ -14,7 +38,7 @@ class App extends React.Component {
             <div className='col-12 text-center'>
               <h1 className='fw-bold'>FEEDUITEN APPS</h1>
               <hr className='w-75 mx-auto' />
-              <h2 className='fw-bold'>Rp 1.500.000,-</h2>
+              <h2 className='fw-bold'>Rp {this.state.sisaUang},-</h2>
               <span className='title-md'>Sisa uang kamu tersisa 75% lagi</span>
             </div>
           </div>
@@ -26,7 +50,7 @@ class App extends React.Component {
                   <i class="bi bi-wallet2"></i>
                 </div>
                 <span className='title-sm'>Pemasukan</span>
-                <h3 className='fw-bold'>Rp 2.000.000,-</h3>
+                <h3 className='fw-bold'>Rp {this.state.pemasukanUang},-</h3>
                 <div>
                   <span className='title-sm text-ungu fw-bold'>50</span><span className='title-sm'> Transaksi</span>
                 </div>
@@ -37,8 +61,8 @@ class App extends React.Component {
                 <div className='icon-wrapper mb-1'>
                   <i class="bi bi-cash-stack"></i>
                 </div>
-                <span className='title-sm'>Pemasukan</span>
-                <h3 className='fw-bold'>Rp 2.000.000,-</h3>
+                <span className='title-sm'>Pengeluaran</span>
+                <h3 className='fw-bold'>Rp {this.state.pengeluaranUang},-</h3>
                 <div>
                   <span className='title-sm text-ungu fw-bold'>50</span><span className='title-sm'> Transaksi</span>
                 </div>
@@ -57,19 +81,24 @@ class App extends React.Component {
           </div>
 
           <div className='row mt-4'>
-            <div className='col-12 d-flex justify-content-between align-items-center'>
-              <div className='d-flex align-items-center'>
-                <div className='icon-wrapper'>
-                  <i class="bi bi-wallet2"></i>
-                </div>
-                <div className='transaction ms-3 d-flex flex-column'>
-                  <h6>Menerima gaji</h6>
-                  <span className='title-sm'>1 July 2022</span>
-                </div>
-              </div>
+            {this.state.summary.map((sum) => {
+              return (
+                <div className='col-12 mb-3 d-flex justify-content-between align-items-center'>
+                  <div className='d-flex align-items-center'>
+                    <div className={sum.kategori === 'IN' ? 'icon-wrapper-in' : 'icon-wrapper-out'}>
+                      <i class={sum.kategori === 'IN' ? 'bi bi-wallet2' : 'bi bi-bag-dash'}></i>
+                    </div>
+                    <div className='transaction ms-3 d-flex flex-column'>
+                      <h6>{sum.deskripsi}</h6>
+                      <span className='title-sm'>{sum.tanggal}</span>
+                    </div>
+                  </div>
 
-              <h5 className='text-money-in'>Rp 1000.000</h5>
-            </div>
+                  <h5 className={sum.kategori === 'IN' ? 'text-money-in' : 'text-money-out'} >Rp {sum.nominal}</h5>
+                </div>
+              )
+            })}
+
           </div>
         </div>
       </>
